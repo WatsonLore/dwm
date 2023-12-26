56afe11477094515e850163d31a9d2b50eb18dbd
WatsonLore <bp.baudelaire@gmail.com>
2023-12-26 01:53:34 +0000

    Bettter Binds

--- vanitygaps.c
+++ vanitygaps.c
@@ -16,8 +16,6 @@ static void centeredfloatingmaster(Monitor *m);
 static void deck(Monitor *m);
 static void dwindle(Monitor *m);
 static void fibonacci(Monitor *m, int s);
-static void grid(Monitor *m);
-static void nrowgrid(Monitor *m);
 static void spiral(Monitor *m);
 static void tile(Monitor *m);
 /* Internals */
@@ -626,35 +624,6 @@ gaplessgrid(Monitor *m)
  * Gridmode layout + gaps
  * https://dwm.suckless.org/patches/gridmode/
  */
-void
-grid(Monitor *m)
-{
-	unsigned int i, n;
-	int cx, cy, cw, ch, cc, cr, chrest, cwrest, cols, rows;
-	int oh, ov, ih, iv;
-	Client *c;
-
-	getgaps(m, &oh, &ov, &ih, &iv, &n);
-
-	/* grid dimensions */
-	for (rows = 0; rows <= n/2; rows++)
-		if (rows*rows >= n)
-			break;
-	cols = (rows && (rows - 1) * rows >= n) ? rows - 1 : rows;
-
-	/* window geoms (cell height/width) */
-	ch = (m->wh - 2*oh - ih * (rows - 1)) / (rows ? rows : 1);
-	cw = (m->ww - 2*ov - iv * (cols - 1)) / (cols ? cols : 1);
-	chrest = (m->wh - 2*oh - ih * (rows - 1)) - ch * rows;
-	cwrest = (m->ww - 2*ov - iv * (cols - 1)) - cw * cols;
-	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		cc = i / rows;
-		cr = i % rows;
-		cx = m->wx + ov + cc * (cw + iv) + MIN(cc, cwrest);
-		cy = m->wy + oh + cr * (ch + ih) + MIN(cr, chrest);
-		resize(c, cx, cy, cw + (cc < cwrest ? 1 : 0) - 2*c->bw, ch + (cr < chrest ? 1 : 0) - 2*c->bw, False);
-	}
-}
 
 /*
  * Horizontal grid layout + gaps
@@ -725,59 +694,6 @@ horizgrid(Monitor *m) {
  * nrowgrid layout + gaps
  * https://dwm.suckless.org/patches/nrowgrid/
  */
-void
-nrowgrid(Monitor *m)
-{
-	unsigned int n;
-	int ri = 0, ci = 0;  /* counters */
-	int oh, ov, ih, iv;                         /* vanitygap settings */
-	unsigned int cx, cy, cw, ch;                /* client geometry */
-	unsigned int uw = 0, uh = 0, uc = 0;        /* utilization trackers */
-	unsigned int cols, rows = m->nmaster + 1;
-	Client *c;
-
-	/* count clients */
-	getgaps(m, &oh, &ov, &ih, &iv, &n);
-
-	/* nothing to do here */
-	if (n == 0)
-		return;
-
-	/* force 2 clients to always split vertically */
-	if (FORCE_VSPLIT && n == 2)
-		rows = 1;
-
-	/* never allow empty rows */
-	if (n < rows)
-		rows = n;
-
-	/* define first row */
-	cols = n / rows;
-	uc = cols;
-	cy = m->wy + oh;
-	ch = (m->wh - 2*oh - ih*(rows - 1)) / rows;
-	uh = ch;
-
-	for (c = nexttiled(m->clients); c; c = nexttiled(c->next), ci++) {
-		if (ci == cols) {
-			uw = 0;
-			ci = 0;
-			ri++;
-
-			/* next row */
-			cols = (n - uc) / (rows - ri);
-			uc += cols;
-			cy = m->wy + oh + uh + ih;
-			uh += ch + ih;
-		}
-
-		cx = m->wx + ov + uw;
-		cw = (m->ww - 2*ov - uw) / (cols - ci);
-		uw += cw + iv;
-
-		resize(c, cx, cy, cw - (2*c->bw), ch - (2*c->bw), 0);
-	}
-}
 
 /*
  * Default tile layout + gaps
